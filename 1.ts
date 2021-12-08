a43fb6092d540a2ccc5700bb6786b4b1d57c9e6f
Łukasz Kółko <kolkolukasz@gmail.com>
2021-12-08 20:49:36 +0100

    First day

---
+++ 1.ts
@@ -0,0 +1,41 @@
+import fs from 'fs'
+
+const sonarData: number[] = fs
+  .readFileSync('1.txt')
+  .toString()
+  .split('\n')
+  .map((x: string): number => parseInt(x))
+  .filter((x: number): number => x)
+
+const countNumberOfIncreasesDepths = (data: number[]) => {
+  let numberOfIncreasesDepths = 0
+
+  data.reduce((current: number, next: number): number => {
+    if (next > current) numberOfIncreasesDepths += 1
+    return next
+  })
+
+  return numberOfIncreasesDepths
+}
+
+const convertToThreeMeasurementSlidingWindows = (data: number[]): number[] => {
+  let sums: number[] = []
+
+  for (let i = 0; i < data.length; i++) {
+    const a = data[i]
+    const b = data[i + 1]
+    const c = data[i + 2]
+
+    if (b && c) {
+      sums.push(a + b + c)
+    }
+  }
+
+  return sums
+}
+
+const partOne = countNumberOfIncreasesDepths(sonarData)
+console.log('Part one result:', partOne)
+
+const partTwo = countNumberOfIncreasesDepths(convertToThreeMeasurementSlidingWindows(sonarData))
+console.log('Part two result:', partTwo)
---
+++ README.md
@@ -0,0 +1,19 @@
+# Advent Of Code 2021
+
+## Source
+
+https://adventofcode.com/
+
+## Requirements
+
+- npm
+
+## Usage
+
+- Firstly run `npm install`
+
+- To run specific file use `npm run start -- FILENAME`
+
+  For example `npm run start -- 1.ts`
+
+- To run specific file in watch mode use `npm run start:w -- FILENAME`
