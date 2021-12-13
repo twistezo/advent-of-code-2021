94365f3d4f43dea30b508558949a2b6e45ddf71c
Łukasz Kółko <kolkolukasz@gmail.com>
2021-12-13 17:33:49 +0100

    5 b

--- days/5.ts
+++ days/5.ts
@@ -33,55 +33,52 @@ const linesOfVents: Line[] = fs
   )
   .slice(0, -1)
 
-const cleanedLinesOfVents = [...linesOfVents].filter(
-  (line: Line) => line.start.x === line.end.x || line.start.y === line.end.y
+// HW: Horizontal and Vertical
+const linesHW = [...linesOfVents].filter(
+  (line: Line): boolean =>
+    line.start.x === line.end.x || line.start.y === line.end.y
 )
 
-const boarSideSize = Math.pow(
-  10,
-  cleanedLinesOfVents[0].start.x.toString().length
-)
-const board: Board = Array.from(Array(boarSideSize), () =>
-  new Array(boarSideSize).fill('.')
-)
-
-const draw = (lines: Line[], board: Board): Board => {
-  getAllPoints(lines).forEach((point: Point, i) => {
-    let pointOnBoard = board[point.y][point.x]
-
-    if (pointOnBoard === '.') {
-      board[point.y][point.x] = 1
-    } else {
-      board[point.y][point.x] += pointOnBoard
-    }
-  })
-
-  return board
+const slope = (line: Line): number => {
+  const { start, end } = line
+  return (end.y - start.y) / (end.x - start.x)
 }
 
-const getAllPoints = (lines: Line[]): Point[] => {
+// D: Diagonal
+const linesD = [...linesOfVents].filter(
+  (line: Line) => Math.abs(slope(line)) === 1
+)
+
+const boarSideSize = Math.pow(10, linesHW[0].start.x.toString().length)
+const board: Board = Array.from(Array(boarSideSize), () =>
+  new Array(boarSideSize).fill(0)
+)
+
+const getHWPoints = (lines: Line[]): Point[] => {
   let points: Point[] = []
   lines.forEach((line: Line) => {
-    if (line.start.x === line.end.x) {
-      const min = Math.min(line.start.y, line.end.y)
-      const max = Math.max(line.start.y, line.end.y)
+    const { start, end } = line
+
+    if (start.x === end.x) {
+      const min = Math.min(start.y, end.y)
+      const max = Math.max(start.y, end.y)
       const yPoints = getNumbersBetween(min, max)
 
       yPoints.forEach((n: number) => {
         points.push({
-          x: line.start.x,
+          x: start.x,
           y: n,
         })
       })
-    } else if (line.start.y === line.end.y) {
-      const min = Math.min(line.start.x, line.end.x)
-      const max = Math.max(line.start.x, line.end.x)
+    } else if (start.y === end.y) {
+      const min = Math.min(start.x, end.x)
+      const max = Math.max(start.x, end.x)
       const xPoints = getNumbersBetween(min, max)
 
       xPoints.forEach((n: number) => {
         points.push({
           x: n,
-          y: line.start.y,
+          y: start.y,
         })
       })
     }
@@ -99,11 +96,37 @@ const getNumbersBetween = (min: number, max: number): number[] => {
   return numbers
 }
 
+const getDPoints = (lines: Line[]): Point[] => {
+  let points: Point[] = []
+  lines.forEach((line: Line) => {
+    const { start, end } = line
+    const slope: number = (end.y - start.y) / (end.x - start.x)
+    const intercept = start.y - slope * start.x
+    const minX = Math.min(start.x, end.x)
+    const maxX = Math.max(start.x, end.x)
+
+    for (let x = minX; x <= maxX; x++) {
+      const y = slope * x + intercept
+      points.push({ x, y })
+    }
+  })
+
+  return points
+}
+
+const drawPoints = (points: Point[], board: Board): Board => {
+  points.forEach((point: Point) => {
+    board[point.y][point.x] += 1
+  })
+
+  return board
+}
+
 const numberOfOverlappedPoints = (board: Board): number => {
   let overlappedPoints = 0
   board.forEach(row => {
     row.forEach(number => {
-      if (number !== '.' && number > 1) {
+      if (number >= 2) {
         overlappedPoints += 1
       }
     })
@@ -112,7 +135,12 @@ const numberOfOverlappedPoints = (board: Board): number => {
   return overlappedPoints
 }
 
-console.log(
-  'Part one result:',
-  numberOfOverlappedPoints(draw(cleanedLinesOfVents, [...board]))
-)
+const pointsHW = getHWPoints(linesHW)
+const boardHW = drawPoints(pointsHW, [...board])
+const overlappedHW = numberOfOverlappedPoints([...boardHW])
+console.log('Part one result:', overlappedHW)
+
+const pointsD = getDPoints(linesD)
+const boardHWD = drawPoints(pointsD, [...boardHW])
+const overlappedHWD = numberOfOverlappedPoints([...boardHWD])
+console.log('Part two result:', overlappedHWD)
